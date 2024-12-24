93f0c828913bdbba91fdbb4085a00d34d07d53a8
raksha-rajpoot <rakraj654@gmail.com>
2024-12-24 18:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  const uniqueSorted = [...new Set(arr)].sort((a, b) => b - a);
+
+  return uniqueSorted.length < 2 ? -Infinity : uniqueSorted[1];
+}
